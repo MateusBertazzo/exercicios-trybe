313dad3f1d772caaf5e9e7010ca44e638c87a054
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-11-23 18:38:14 -0300

    exercicio 3

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-01-javascript-primeirospassos/exercicio3.js
@@ -0,0 +1,12 @@
+const a = 44
+const b = 12
+const c = 32
+
+
+if (a > b && a > c){
+    console.log("A é o maior")
+} else if (b > a && b > c) {
+    console.log("B é o maior")
+} else {
+    console.log("C é o maior")
+}

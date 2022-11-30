c4840d698f985d452b1a5903ec96af5963ec5fb3
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-11-30 15:41:03 -0300

    first commit

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objeto-e-funcao/aula1.js
@@ -0,0 +1,13 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medal: {golden: 2, silver: 3}
+}
+
+console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`)
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log(`A jogadora possui ${player.medal.golden} medalhas de Ouro e ${player.medal.silver} de Prata` )
+

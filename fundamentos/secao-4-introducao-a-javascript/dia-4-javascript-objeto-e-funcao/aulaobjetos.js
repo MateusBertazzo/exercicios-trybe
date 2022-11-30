e16835b30248ddf582fd4b0836ec1bf966911b08
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-11-30 17:23:28 -0300

    exercicios em aula

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objeto-e-funcao/aulaobjetos.js
@@ -0,0 +1,30 @@
+// let mcItems = {
+//   'Big Mac': 20,
+//   'Big Tasty': 30, 
+// };
+
+let person = {
+  firstName: 'Amanda',
+  lastName: 'Aragao',
+  canDrive: true,
+  age: 19,
+  favoriteMovies: ['Interestelar', 'Harry Potter','Senhor dos aneis'],
+  adress: {
+    city: 'São Paulo',
+    street: 'Rua Dalvo Trombeta',
+    number: 357,
+  }
+};
+
+// ache o terceiro filme da amanda
+// console.log(person.favoriteMovies[2])
+
+// E se eu quiser adicionar uma propriedade ? 
+// city nao existia antes, e quando eu precisar adicionar, basta chamar a chave e reatribuir a um novo valor
+
+// como descobrir o tipo de uma propriedade usando o typeof()
+for (let key in person) {
+  console.log(key , person[key])
+}
+
+// console.log(`${person[key]}`)

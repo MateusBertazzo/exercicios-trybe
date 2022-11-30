37a67a73527cce95730f05977361c9a4cfd71f86
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-11-30 16:21:47 -0300

    resovendo exercicios do couse

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objeto-e-funcao/forIn.js
@@ -0,0 +1,36 @@
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(`${index} - ${cars[index]}`);
+// }
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// for (let index in car) {
+//   console.log(`${index} - ${car[index]}`);
+// }
+
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge',
+// };
+
+// for(let index in names){
+//   console.log(`Olá, ${names[index]}`)
+// }
+
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index in car) {
+  console.log(`${index}: ${car[index]}`);
+}

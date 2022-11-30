a37eb1700b6388d0f470d6c4f8816d051afb53d9
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-11-30 18:04:10 -0300

    resolvendo exercicios in aula

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objeto-e-funcao/aulaFunction.js
@@ -0,0 +1,36 @@
+// function sum (num1, num2) {
+//   return num1 + num2
+// }
+
+// let xablau = sum(7, 10);
+
+// console.log(xablau)
+
+
+// function biggest (firstNumber, secondNumber) {
+//   if(firstNumber > secondNumber){
+//     return 'O maior número é o : ' + firstNumber
+//   } else if (firstNumber === secondNumber){
+//     return 'Empate'
+//   } else {
+//     return 'O maior número é o : ' + secondNumber
+//   }
+// }
+
+// console.log(biggest(10,64))
+
+
+// function average (num1,num2,num3) {
+//   return (num1 + num2 + num3) / 3;
+// }
+
+// console.log(average(1000,20,30));
+
+// GERE UM ARRAY COM 100 CARACTRES IGUAIS
+
+function generateArray (character,number,) {
+  let charactersArray = []
+  for(let index = 0; index < number;) {
+    
+  }
+}

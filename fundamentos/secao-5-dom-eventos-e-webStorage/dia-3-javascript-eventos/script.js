63f5a176540a738505be9b3524e38976bf91508d
Mateus Bertazzo <mateusbataglin52@gmail.com>
2022-12-13 10:56:01 -0300

    adicionada marcação nos feriados

--- fundamentos/secao-5-dom-eventos-e-webStorage/dia-3-javascript-eventos/script.js
+++ fundamentos/secao-5-dom-eventos-e-webStorage/dia-3-javascript-eventos/script.js
@@ -49,3 +49,22 @@ function createHolidayButton(buttonName) {
 
 createHolidayButton('Feriados');
 
+const displayHolidays = () => {
+  let getHolidayButton = document.querySelector('#btn-holiday');
+  let getHolidays = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let setNewColor = 'yellow';
+
+  getHolidayButton.addEventListener('click', () => {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+      if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+      }
+    }
+  });
+}
+
+displayHolidays();
+

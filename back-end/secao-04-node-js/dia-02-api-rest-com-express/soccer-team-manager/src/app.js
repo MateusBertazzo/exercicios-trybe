30ec7db6ceab65863b6bc0ed9bf5aafcca682372
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-06-26 09:47:16 -0300

    resolve exercicio dia2 secao 4

--- back-end/secao-04-node-js/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
+++ back-end/secao-04-node-js/dia-02-api-rest-com-express/soccer-team-manager/src/app.js
@@ -2,6 +2,32 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (request, response) => response.status(200).json({ message: 'Olá mundo' }));
+
+app.get('/teams', (request, response) => response.status(200).json({ teams }));
+
+app.post('/teams', (request, response) => {
+  const newTeam = { ...request.body };
+  teams.push(newTeam);
+
+  response.status(201).json({ team: newTeam });
+});
+
 module.exports = {
   app,
 };
